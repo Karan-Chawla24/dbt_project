94e51646d58da66a0289aa682a21b2df10ada1c1
KaranChawla <chawlakaran663@gmail.com>
2024-07-02 14:23:55 +0530

    did changes in conf

--- models/example_model.sql
+++ models/example_model.sql
@@ -1,4 +1,4 @@
 with customer_demo_cte as (
-    select * from {{ source('customer_data', 'customer_demo') }}
+    select * from {{ source('demo', 'customer_demo') }}
 )
 select * from customer_demo_cte
