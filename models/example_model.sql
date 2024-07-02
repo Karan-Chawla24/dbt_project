2d459674ab05cdb96aba957a520031208efae14f
KaranChawla <chawlakaran663@gmail.com>
2024-07-02 14:32:50 +0530

    update sql file

--- models/example_model.sql
+++ models/example_model.sql
@@ -1,4 +1,4 @@
 with customer_demo_cte as (
     select * from {{ source('demo', 'customer_demo') }}
 )
-select * from "DEMO"."PUBLIC"."customer_demo";
+select * from "DEMO"."PUBLIC"."customer_demo"
